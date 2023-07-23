13520fda170495e564981a5fbc923882419d75f7
GrayThinker <jshatti00@gmail.com>
2023-07-23 13:34:22 -0700

    Add bedroom sd images

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+main() {
+
+    int num {};
+    cout << "Enter: ";
+    cin >> num;
+    cout << "you entered " num << endl;
+
+    count << "hello" << endl;
+
+    return 0;
+}
